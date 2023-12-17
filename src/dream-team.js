6e7f8be191ab416d25cadf89e715ad8c483b0b73
Jezhora <jezhora.photo@gmail.com>
2023-12-17 21:54:05 +0300

    feat: 47 passed

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  return members.map(member => {
+    if (typeof member === 'string') {
+      return member.replace(/ /g, '').slice(0, 1).toUpperCase();
+    }
+  })
+  .sort()
+  .join('');
 }
+// console.log(createDreamTeam([
+//   ['David Abram'],
+//   ['Robin Attfield'],
+//   'Thomas Berry',
+//   ['Paul R.Ehrlich'],
+//   'donna Haraway',
+//   ' BrIaN_gOodWiN  ',
+//   {
+//     0: 'Serenella Iovino'
+//   },
+//   'Erazim Kohak',
+//   '  val_plumwood',
+// ]))
 
 module.exports = {
   createDreamTeam
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,90 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let string;
+  if (typeof str !== 'string') {
+    string = String(str);
+  } else {
+    string = str;
+  }
+  
+  const strRepeat = options.repeatTimes ? options.repeatTimes : false;
+  const strSeparator = options.separator;
+  const strAddRepeat = options.additionRepeatTimes ? options.additionRepeatTimes : false;
+  const strAddSeparator = options.additionSeparator;
+
+  let strAdd;
+  
+  for (let key in options) {
+    if (key === 'addition') {
+      strAdd = options.addition;
+      if (typeof options.addition !== 'string'
+      || options.addition === false
+      || options.addition === NaN
+      || options.addition === Infinity
+      || options.addition === null) {
+        strAdd = String(options.addition);
+      }
+      console.log('!')
+    }
+  }
+
+  let addition;
+  let stringToRepeat;
+  let result;
+
+  if (strAdd) { 
+    if (strAddSeparator) {
+      if(strAddRepeat) {
+        addition = `${strAdd}${strAddSeparator}`
+        .repeat(strAddRepeat).slice(0, -strAddSeparator.length)
+      } else {
+        addition = `${strAdd}${strAddSeparator}`.slice(0, -strAddSeparator.length);
+      }
+  
+    } else {
+      if (strAddRepeat) {
+      addition = `${strAdd}|`.repeat(strAddRepeat).slice(0,-1);
+      } else {
+        addition = `${strAdd}|`.slice(0,-1);
+      }
+    }
+  }
+
+  if (strSeparator) { 
+    if(addition) {
+      stringToRepeat = `${string}${addition}${strSeparator}`;
+    } else {
+      stringToRepeat = `${string}${strSeparator}`
+    }
+  
+    if(strRepeat) {
+      result = stringToRepeat.repeat(strRepeat).slice(0, -strSeparator.length);
+    } else {
+      result = stringToRepeat.slice(0, -strSeparator.length);
+    }
+
+  } else {
+    if(addition) {
+      stringToRepeat = `${string}${addition}+`;
+    } else {
+      stringToRepeat = `${string}+`
+    }
+    if (strRepeat) {
+      result = stringToRepeat.repeat(strRepeat).slice(0,-1);
+    } else {
+      result = stringToRepeat.slice(0,-1);
+    }
+  } 
+
+  return result;
 }
+// const objWithSpecificCoercion = {
+//   [Symbol.toPrimitive]: hint => hint !== 'number' ? 'STRING_OR_DEFAULT' : 'NUMBER'
+// };
+
+// repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' })
 
 module.exports = {
   repeater
